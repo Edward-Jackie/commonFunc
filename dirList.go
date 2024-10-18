b37fdbc1b0da24cdb7f7e4da091db3955589d081
Ed-j <419130032@qq.com>
2024-10-18 20:12:15 +0800

    basic init

---
+++ dirList.go
@@ -0,0 +1,23 @@
+package main
+
+var web_dirs = []string{
+	"handler",
+	"models",
+	"service",
+	"dto",
+	"grpc",
+}
+
+var basic_dir = []string{
+	"config",
+	"backup",
+	"cmd",
+	"logs",
+	"common",
+	"internal",
+	"test",
+}
+
+var internal_dir = []string{
+	"port",
+}
---
+++ frameList.go
@@ -0,0 +1,15 @@
+package main
+
+var webFrameList = []string{
+	"echo",
+	"gin",
+}
+
+var dbFrameList = []string{
+	"gorm",
+	"xorm",
+}
+
+var cacheFrameList = []string{
+	"redis",
+}
